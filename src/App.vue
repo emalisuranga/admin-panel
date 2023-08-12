323a6d73a6abf72d2f66724860f93627f17f3186
emalisuranga <37981046+emalisuranga@users.noreply.github.com>
2023-08-13 05:05:10 +0900

    init//make side bar

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app id="inspire">
+ 
+    <Sidebar />
+    
+  </v-app>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  import Sidebar from '@/components/Sidebar.vue'
+
+  const cards = ['Today', 'Yesterday']
+
+  const drawer = ref(null)
+</script>
+
+<script>
+  export default {
+    data: () => ({
+      cards: ['Today', 'Yesterday'],
+      drawer: null,
+      links: [
+        ['mdi-inbox-arrow-down', 'Inbox'],
+        ['mdi-send', 'Send'],
+        ['mdi-delete', 'Trash'],
+        ['mdi-alert-octagon', 'Spam'],
+      ],
+    }),
+  }
+</script>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,46 @@
+<template lang="">
+    <div>
+      <v-navigation-drawer v-model="drawer">
+      <v-sheet
+        color="grey-lighten-4"
+        class="pa-4"
+      >
+        <v-avatar
+          class="mb-4"
+          color="grey-darken-1"
+          size="64"
+        ></v-avatar>
+
+        <div>john@google.com</div>
+      </v-sheet>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+          v-for="[icon, text] in links"
+          :key="icon"
+          :prepend-icon="icon"
+          :title="text"
+          link
+        ></v-list-item>
+      </v-list>
+    </v-navigation-drawer> 
+    </div>
+</template>
+<script>
+export default {
+  name: "Sidebar",
+  data: () => ({
+      drawer: null,
+      links: [
+        ['mdi-microsoft-windows', 'Dashboard'],
+        ['mdi-clipboard-list-outline', 'Facebook Job Seeker'],
+        ['mdi-delete', 'Business Process'],
+        ['mdi-alert-octagon', 'System Setting'],
+      ],
+    }),
+};
+</script>
+<style lang="">
+</style>
