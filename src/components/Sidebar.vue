400f23a766ff9356ec683b3e86f996f4b0db0d88
emalisuranga <37981046+emalisuranga@users.noreply.github.com>
2023-08-31 04:37:36 +0900

    change side bar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -18,12 +18,13 @@
 
       <v-list>
         <v-list-item
-          v-for="[icon, text] in links"
-          :key="icon"
-          :prepend-icon="icon"
-          :title="text"
-          link
-        ></v-list-item>
+          v-for="link in links"
+          :key="link.icon"
+          :prepend-icon="link.icon"
+          :title="link.label"
+          @click="navigateTo(link.path)"
+          link>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer> 
     </div>
@@ -33,13 +34,26 @@ export default {
   name: "Sidebar",
   data: () => ({
       drawer: null,
-      links: [
+      links1: [
         ['mdi-microsoft-windows', 'Dashboard'],
         ['mdi-clipboard-list-outline', 'Facebook Job Seeker'],
         ['mdi-delete', 'Business Process'],
         ['mdi-alert-octagon', 'System Setting'],
       ],
+      links: [
+        { path: '/', label: 'Dashboard',icon: 'mdi-microsoft-windows'},
+        { path: '/dashboard', label: 'Facebook Job Seeker',icon: 'mdi-clipboard-list-outline'},
+        { path: '/', label: 'Business Process',icon: 'mdi-delete'},
+        { path: '/', label: 'System Setting',icon: 'mdi-alert-octagon'},
+        // Add more links as needed
+      ]
     }),
+  methods: {
+    navigateTo(path) {
+      // Use Vue Router's programmatically navigation here
+      this.$router.push(path);
+    }
+  }
 };
 </script>
 <style lang="">
