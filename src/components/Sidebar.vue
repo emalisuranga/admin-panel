61748bd9085b6ae605466c4f3ce5ef5c613b49fc
emalisuranga <37981046+emalisuranga@users.noreply.github.com>
2023-09-06 10:38:51 +0900

    tow way data binding for sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,6 @@
 <template lang="">
     <div>
-      <v-navigation-drawer v-model="drawer">
+      <v-navigation-drawer v-model="drawerState">
       <v-sheet
         color="grey-lighten-4"
         class="pa-4"
@@ -29,25 +29,47 @@
     </v-navigation-drawer> 
     </div>
 </template>
+<script setup>
+import { reactive } from 'vue';
+import { ref,watch } from 'vue';
+
+
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const drawerState = ref(props.modelValue);
+
+watch(
+  () => props.modelValue,
+  (newValue) => {
+    drawerState.value = newValue;
+  }
+);
+
+</script>
 <script>
 export default {
   name: "Sidebar",
   data: () => ({
-      drawer: null,
-      links1: [
-        ['mdi-microsoft-windows', 'Dashboard'],
-        ['mdi-clipboard-list-outline', 'Facebook Job Seeker'],
-        ['mdi-delete', 'Business Process'],
-        ['mdi-alert-octagon', 'System Setting'],
-      ],
-      links: [
-        { path: '/', label: 'Dashboard',icon: 'mdi-microsoft-windows'},
-        { path: '/candidate', label: 'Facebook Job Seeker',icon: 'mdi-clipboard-list-outline'},
-        { path: '/', label: 'Business Process',icon: 'mdi-delete'},
-        { path: '/', label: 'System Setting',icon: 'mdi-alert-octagon'},
-        // Add more links as needed
-      ]
-    }),
+    drawer: true,
+    links1: [
+      ['mdi-microsoft-windows', 'Dashboard'],
+      ['mdi-clipboard-list-outline', 'Facebook Job Seeker'],
+      ['mdi-delete', 'Business Process'],
+      ['mdi-alert-octagon', 'System Setting'],
+    ],
+    links: [
+      { path: '/', label: 'Dashboard', icon: 'mdi-microsoft-windows' },
+      { path: '/candidate', label: 'Facebook Job Seeker', icon: 'mdi-clipboard-list-outline' },
+      { path: '/', label: 'Business Process', icon: 'mdi-delete' },
+      { path: '/', label: 'System Setting', icon: 'mdi-alert-octagon' },
+      // Add more links as needed
+    ]
+  }),
   methods: {
     navigateTo(path) {
       // Use Vue Router's programmatically navigation here
